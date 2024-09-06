64c2d0c9a8781ba556b4cdde67fb9909ad0a223c
AlexisJimenez14 <a.22308051281213@cbtis128.edu.mx>
2024-09-06 09:32:18 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,38 @@
+"""Hola soy alexis 
+trabajando candenasa"""
+'''auuuuuuuuuuu'''
+
+nombre="Alexis"
+print(nombre[0])
+print(nombre)
+for c in "Alexis":
+  print(c)
+
+MisNovias=["agripina","Anastacia","maria"]
+
+for chicas in MisNovias:
+  print(chicas)
+
+
+print("otro ejemplo")
+
+b = "alexis jimenez"
+
+"""primero se guarda un texto en una variable no definida pero actua como string
+se imprime la variable b pero con una condicion añadida por un vector que dice que 
+adelante del caracter 2 empezando desde el 0 
+imprimira hasta el caracter 5"""
+
+print(b[2:5])
+
+
+print(" a mayusculas")
+a = "alexis"
+print(a.upper())
+
+print("concantenar")
+a = "kim"
+b = "rios"
+c = a +"_"+b
+print(c)
+
---
+++ mivariables.py
@@ -0,0 +1,18 @@
+import math
+edad=17  #mi primer codigo
+nombre="Alexis Fabian Jimenez Jimenez"
+print("tu edad es ",edad)
+print("tu nombre es ",nombre)
+print("-- tipo de variables ---")
+
+x = str(7)    # x will be '3'
+y = int(3.234567890)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x)
+print(y)
+print(z)
+print("--funciones matematicas--")
+
+x= math.sqrt(64)
+print(x)

a3b23d09af259b296139a111d814fd8767043b63
AlexisJimenez14 <a.22308051281213@cbtis128.edu.mx>
2024-09-06 09:57:26 -0600

    Add files via upload

---
+++ ejemploif.py
@@ -0,0 +1,6 @@
+edad=15 #asigna un valor 17
+if edad >= 18 : #ejecuta la decision evaluando los 2 valores, si es mayor a 18 la variable edad verdadero pero si es menor sera falso
+    print("puedes entrar a bailar")
+else: 
+    print("vete a dormir...zzsss")    
+
